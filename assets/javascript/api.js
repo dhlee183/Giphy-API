1858f5e6186c2abbad924115a505a7799f47fa9c
David Hyun Lee <David Hyun Lee>
2016-09-11 23:29:03 -0400

    Updated the js file to include gif pause and play functionality

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,39 +1,65 @@
 $(document).ready(function() {
 
-		var animals = ['dog', 'cat', 'rabbit', 'hamster', 'skunk', 'goldfish', 'bird', 'ferret', 'turtle', 
-						'sugar glider', 'chinchilla', 'hedgehog', 'hermit crab', 'gerbil', 'pygmy goat',
-						'chicken', 'capybara', 'teacup pig', 'serval', 'salamander', 'frog'];
+	var animals = ['dog', 'cat', 'rabbit', 'hamster', 'skunk', 'goldfish', 'bird', 'ferret', 'turtle', 
+					'sugar glider', 'chinchilla', 'hedgehog', 'hermit crab', 'gerbil', 'pygmy goat',
+					'chicken', 'capybara', 'teacup pig', 'serval', 'salamander', 'frog'];
 
-		function displayAnimalInfo(){
+	function displayAnimalInfo(){
 
-		var animal = $(this).attr('data-name');
-		var queryURL = 'http://api.giphy.com/v1/gifs/search?q='  + animal + "&y=&limit=10&api_key=dc6zaTOxFJmzC";
+	var animalSelection = $(this).attr('data-name');
+	var queryURL = 'http://api.giphy.com/v1/gifs/search?q='  + animalSelection + "&y=&limit=10&api_key=dc6zaTOxFJmzC";
 		
-		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-			console.log(response);
+	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+		console.log(response);
 
-			$('#animalsView').empty()
+		$('#animalsView').empty()
 
-			for (i = 0; i < response.data.length; i++) {
+		for (i = 0; i < response.data.length; i++) {
 
-			var animalDiv = $('<div class="animal">');
+		var animalDiv = $('<div class="animalInfo">');
 
-			var rating = response.data[i].rating;
+		var rating = response.data[i].rating;
 
-			var p = $('<p>').text( "Rating: " + rating);
+		var p = $('<p>').text( "Rating: " + rating);
 
-			animalDiv.append(p);
+		animalDiv.append(p);
 
-			var image = $('<img>').attr("src", response.data[i].images.original.url);
+		var img = $('<img>')
 
-			animalDiv.append(image);
+		img.attr("src", response.data[i].images.original_still.url);
 
-			$('#animalsView').prepend(animalDiv);
+		img.attr("data-still", response.data[i].images.original_still.url);
 
-			}
-		});
+		img.attr("data-animate", response.data[i].images.original.url);
 
-	}
+		img.attr("data-state", "still");
+
+		img.addClass('animalImg')
+
+		animalDiv.append(img);
+
+		$('#animalsView').prepend(animalDiv);
+
+		}
+	});
+
+	};
+
+	$(document).on('click', '.animalImg', function() {
+				
+		var state = $(this).attr('data-state');
+
+		if (state == "still") {
+			$(this).attr("src", $(this).data("animate"));
+			$(this).attr("data-state", "animate");
+		}
+		else {
+			$(this).attr("src", $(this).data("still"));
+			$(this).attr("data-state", "still");
+		}
+
+	});
+
 
 	function renderButtons(){ 
 
@@ -48,16 +74,16 @@ $(document).ready(function() {
 		    a.attr('data-name', animals[i]);
 
 		    a.text(animals[i]);
-		     
+
 		    $('#buttonsView').append(a); 
 		}
 	}
 
 	$('#addAnimal').on('click', function(){
 
-		var animal = $('#animal-input').val().trim();
+		var animalPick = $('#animal-input').val().trim();
 
-		animals.push(animal);
+		animals.push(animalPick);
 		
 		renderButtons();
 
